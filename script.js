9be82f5141a30265099eeb5004bb0c2e3401daec
Surr57024 <Visper57024@mail.ru>
2019-11-13 15:31:29 +0300

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll(".column").forEach(Column.process);
+document.querySelectorAll(".note").forEach(Note.process);
+
+/* Кнопка добавление колонки с карточками */
+document
+  .querySelector("[data-action-addColumn]")
+  .addEventListener("click", function(e) {
+    const columnElement = document.createElement("div");
+    columnElement.classList.add("column");
+    columnElement.setAttribute("draggable", "true");
+    columnElement.setAttribute("data-column-id", Column.idCounter++);
+    columnElement.innerHTML = `<div class="column-header"><span class="caption"></span></div>
+      <div class="column-body" data-notes></div>
+      <div class="column-footer">
+        <span data-action-addNote class="action"
+          >+ Добавить карточку</span
+        >
+      </div>`;
+
+    document.querySelector(".columns").append(columnElement);
+    Column.process(columnElement);
+  });
